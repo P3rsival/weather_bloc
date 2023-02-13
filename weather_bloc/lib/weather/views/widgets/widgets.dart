30f046c35c88364ebdfcc09be5c89f4ad3dc7e29
Core <osdel.michel@gmail.com>
2023-02-12 21:26:16 -0500

    weather bloc,cubit

---
+++ weather_bloc/lib/weather/views/widgets/widgets.dart
@@ -0,0 +1,6 @@
+import 'package:weather_bloc/weather/views/widgets/weather_error.dart';
+
+export 'package:weather_bloc/weather/views/widgets/settings_page%20.dart';
+export 'package:weather_bloc/weather/views/widgets/weather_empty%20.dart';
+
+export 'package:weather_bloc/weather/views/widgets/search_page%20.dart';
---
+++ weather_bloc/lib/weather/weather.dart
@@ -0,0 +1,5 @@
+export 'package:weather_bloc/weather/cubit/cubit/weather_cubit.dart';
+export 'package:weather_bloc/weather/views/weather_page.dart';
+export 'package:weather_repository/weather_repository.dart'
+    show WeatherCondition;
+export 'models/models.dart';
